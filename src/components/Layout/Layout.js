14a8c7f1c7935739447b69a40ccb5c4b22ab9598
cayenne-pepper <ju_silva@icloud.com>
2020-01-13 14:43:09 +0100

    Create Navigation component, nav items and work on responsiveness

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => {
   return (
     <div>
       <Toolbar />
+      <SideDrawer />
       <main style={{ marginTop: 72 }}>{props.children}</main>
     </div>
   );
