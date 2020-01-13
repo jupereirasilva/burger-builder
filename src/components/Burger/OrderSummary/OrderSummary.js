9323f93b6cacb06718f30a6b232fb0f6365e4b37
cayenne-pepper <ju_silva@icloud.com>
2020-01-13 11:35:42 +0100

    Add Button component and display price on Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Button from "../../UI/Button/Button";
+
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(
     ingredientKey => {
@@ -16,7 +18,16 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the follwing ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to checkout?</p>
+      <Button clicked={props.purchaseCancelled} btnType="Danger">
+        CANCEL
+      </Button>
+      <Button clicked={props.purchaseContinued} btnType="Success">
+        CONTINUE
+      </Button>
     </div>
   );
 };
