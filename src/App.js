cd8141c75dc2d63dbd153fec9df5f4597abe92b2
cayenne-pepper <ju_silva@icloud.com>
2020-01-15 17:31:26 +0100

    Set up routing and routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -8,10 +9,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Layout />
-        <BurgerBuilder />
-        {/* test */}
-        <Checkout />
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,25 +93,26 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Kizzy",
-        address: {
-          street: "Teststreet 1",
-          zipCode: "765432",
-          country: "Netherlands"
-        },
-        email: "test@test.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(res => this.setState({ loading: false, purchasing: false }))
-      .catch(error => this.setState({ loading: false, purchasing: false }));
+    //   this.setState({ loading: true });
+    //   const order = {
+    //     ingredients: this.state.ingredients,
+    //     price: this.state.totalPrice,
+    //     customer: {
+    //       name: "Kizzy",
+    //       address: {
+    //         street: "Teststreet 1",
+    //         zipCode: "765432",
+    //         country: "Netherlands"
+    //       },
+    //       email: "test@test.com"
+    //     },
+    //     deliveryMethod: "fastest"
+    //   };
+    //   axios
+    //     .post("/orders.json", order)
+    //     .then(res => this.setState({ loading: false, purchasing: false }))
+    //     .catch(error => this.setState({ loading: false, purchasing: false }));
+    this.props.history.push("/checkout");
   };
 
   render() {
